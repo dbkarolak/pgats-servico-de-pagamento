63746a499e9405635cda15cb45d962aa8c2224e1
Denis <deniskarolak@gmail.com>
2026-06-21 00:02:53 -0300

    fix: corrige o primeiro teste, teste de categoria do pagamento, voltando para o valor esperado padrão

--- test/servicoDePagamento.test.js
+++ test/servicoDePagamento.test.js
@@ -12,8 +12,7 @@ describe('Classe de serviço de pagamento', () => {
         const resultado = servicoDePagamento.consultarUltimoPagamento();
 
         //Assert
-        //assert.equal(resultado.categoria,'padrão');
-        assert.equal(resultado.categoria,'cara');
+        assert.equal(resultado.categoria,'padrão');
 
     });
     it('Validar que quando o valor do pagamento for maior que 100, a propriedade categoria é "cara".', () => {
